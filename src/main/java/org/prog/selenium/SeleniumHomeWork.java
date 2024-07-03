4269f633616d2acee9c5416fa01a2e61edea9a77
Pavlo-Savchenko <Spashok117@gmail.com>
2024-07-03 22:00:02 +0300

    DZ5

--- src/main/java/org/prog/selenium/SeleniumHomeWork.java
+++ src/main/java/org/prog/selenium/SeleniumHomeWork.java
@@ -1,5 +1,11 @@
 package org.prog.selenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumHomeWork {
 
     /**
@@ -12,6 +18,17 @@ public class SeleniumHomeWork {
      */
 
     public static void main(String[] args) {
-
+        WebDriver driver = null;
+        try {
+            driver = new ChromeDriver();
+            driver.get("https://allo.ua/");
+            WebElement searchBox = driver.findElement(By.name("search") );
+            searchBox.sendKeys("iphone 15");
+            searchBox.sendKeys(Keys.ENTER);
+        }finally{
+            if(driver!=null){
+                driver.quit();
+            }
+        }
     }
 }
