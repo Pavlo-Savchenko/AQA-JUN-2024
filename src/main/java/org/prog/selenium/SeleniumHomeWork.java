68418bf424c6e1abbf6613cfa398d4a261e7aa88
Pavlo-Savchenko <Spashok117@gmail.com>
2024-07-08 03:00:15 +0300

    DZ6

--- src/main/java/org/prog/selenium/SeleniumHomeWork.java
+++ src/main/java/org/prog/selenium/SeleniumHomeWork.java
@@ -1,5 +1,16 @@
 package org.prog.selenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
 public class SeleniumHomeWork {
 
     /**
@@ -12,6 +23,34 @@ public class SeleniumHomeWork {
      */
 
     public static void main(String[] args) {
+        WebDriver driver = null;
+        String devise = "iphone 15";
+        try {
+            driver.get("https://allo.ua/");
+            WebElement searchBox = driver.findElement(By.name("search") );
+            searchBox.sendKeys(devise);
+            searchBox.sendKeys(Keys.ENTER);
 
+            WebDriverWait driverWait= new WebDriverWait(driver, Duration.ofSeconds(30L));
+            List<WebElement > searchIphones =
+            driverWait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//a[contains(@title,'Apple iPhone 15')]"),5));
+            int g =0;
+            for(WebElement img : searchIphones ) {
+                img.getText();
+                if((img.getText().toUpperCase().contains(devise.toUpperCase()))){
+                    g++;
+                }
+            }
+            if(g>5){
+                System.out.println(devise+ " Знайшло на алло");
+            }else{
+                throw new RuntimeException(devise + " не знайшло");
+            }
+        }finally{
+            if(driver!=null){
+                driver.quit();
+            }
+        }
     }
 }
+
--- src/test/java/org/prog/testng/HomeWorkNG.java
+++ src/test/java/org/prog/testng/HomeWorkNG.java
@@ -1,11 +1,63 @@
 package org.prog.testng;
 
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
 
 public class HomeWorkNG {
 
     //TODO: move your allo.ua test to TestNG Test
     //TODO: make browser start/stop as Before/After suite
     //TODO: print 1st good's price
+    private WebDriver driver;
+    @BeforeSuite
+    public void precondition(){
+    driver = new ChromeDriver();
+    }
+    @AfterSuite
+    public void postCondition(){
+    if(driver!=null){
+        driver.quit();
+    }
+    }
+    @Test
+    public void myTest(){
+    String devise = "iphone 15";
+        driver.get("https://allo.ua/");
+        WebElement searchBox = driver.findElement(By.name("search") );
+        searchBox.sendKeys(devise);
+        searchBox.sendKeys(Keys.ENTER);
+
+        WebDriverWait driverWait= new WebDriverWait(driver, Duration.ofSeconds(30L));
+        List<WebElement > searchIphones =
+                driverWait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//a[contains(@title,'Apple iPhone 15')]"),5));
+        int g =0;
+        for(WebElement img : searchIphones ) {
+            img.getText();
+            if((img.getText().toUpperCase().contains(devise.toUpperCase()))){
+                g++;
+            }
+        }
+        if(g>5){
+            System.out.println(devise+ " Знайшло на алло");
+        }else{
+            throw new RuntimeException(devise + " не знайшло");
+        }
+        List<WebElement> searchPrice=
+                driverWait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//span[contains(@class,'sum')]"),5));
+        System.out.println("перший телефон коштує: "+searchPrice.get(0).getText());
+    }
 }
+
+
