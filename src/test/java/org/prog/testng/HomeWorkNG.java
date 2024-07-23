0332aad51b983954d4639c7c7f74cddf20048e49
Pavlo-Savchenko <Spashok117@gmail.com>
2024-07-23 20:28:34 +0300

    DZ8Demo

--- src/test/java/org/prog/testng/HomeWorkNG.java
+++ src/test/java/org/prog/testng/HomeWorkNG.java
@@ -1,11 +1,83 @@
 package org.prog.testng;
 
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
 
 public class HomeWorkNG {
 
     //TODO: move your allo.ua test to TestNG Test
     //TODO: make browser start/stop as Before/After suite
     //TODO: print 1st good's price
+    private WebDriver driver;
+    @BeforeSuite
+    public void precondition(){
+        driver = new ChromeDriver();
+    }
+    @AfterSuite
+    public void postCondition(){
+        if(driver!=null){
+            driver.quit();
+        }
+    }
+    @Test
+    public void myTest(){
+        String devise = "iphone 15";
+        driver.get("https://allo.ua/");
+        WebElement searchBox = driver.findElement(By.name("search") );
+        searchBox.sendKeys(devise);
+        searchBox.sendKeys(Keys.ENTER);
+
+        WebDriverWait driverWait= new WebDriverWait(driver, Duration.ofSeconds(30L));
+        List<WebElement > searchIphones =
+                driverWait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//a[contains(@title,'Apple iPhone 15')]"),5));
+        int g =0;
+        for(WebElement img : searchIphones ) {
+            img.getText();
+            if((img.getText().toUpperCase().contains(devise.toUpperCase()))){
+                g++;
+            }
+        }
+        if(g>5){
+            System.out.println(devise+ " Знайшло на алло");
+        }else{
+            throw new RuntimeException(devise + " не знайшло");
+        }
+        List<WebElement> searchPrice=
+                driverWait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//div[contains(@class,'v-pb__cur discount')]"),5));
+        System.out.println("перший телефон коштує: "+searchPrice.get(0).getText());
+        WebElement firstElement=searchPrice.get(0);
+        String firstPrice= firstElement.getText();
+
+        List<WebElement> searchPhoneElement=
+                driverWait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//img[contains(@src,'https://i.allo.ua/media/catalog/product/cache/3/small_image/212x184/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone_15')]"),4 ));
+        searchPhoneElement.get(0).click();
+
+        List<WebElement> searchPrice2=
+                driverWait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//span[contains(@class,'sum')]"),2));
+        System.out.println(searchPrice2.get(3).getText());
+        WebElement thirdElement=searchPrice2.get(3);
+        String secondPrice=thirdElement.getText();
+
+
+        WebElement findName = driver.findElement(By.xpath("//h1[contains(@class,'p-view__header-title')]"));
+        WebElement nameElement=findName;
+        String nameEl=nameElement.getText();
+        System.out.println(nameEl );
+
+        Assert.assertEquals(firstPrice, secondPrice,"ціна має бути однакова");
+
+    }
 }
--- src/test/java/org/prog/testng/SqlTests.java
+++ src/test/java/org/prog/testng/SqlTests.java
@@ -1,22 +1,21 @@
 package org.prog.testng;
 
-import io.restassured.RestAssured;
-import io.restassured.specification.RequestSpecification;
-import org.prog.dto.PersonDto;
-import org.prog.dto.ResponseDto;
-import org.testng.annotations.Test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import java.sql.*;
+import java.time.Duration;
+import java.util.List;
 
 public class SqlTests {
-
-    private Connection connection;
-    private Statement statement;
-    private ResultSet resultSet;
-
-    private final static String INSERT_PATTERN = "INSERT INTO Persons (FirstName, LastName, Gender, Title, Nat) " +
-            "VALUES ('%s', '%s', '%s', '%s', '%s')";
-
     //TODO: w3schools : create table
     //TODO: add table to DB with goods name and price
     //TODO: w3schools : insert into table
@@ -24,50 +23,36 @@ public class SqlTests {
     //TODO: w3schools : select from table WHERE + update table
     //TODO: * make test update data in DB if the goods name is already present
 
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
+    private WebDriver driver;
+    @BeforeSuite
+    public void precondition(){
+        driver = new ChromeDriver();
+    }
+    @AfterSuite
+    public void postCondition(){
+        if(driver!=null){
+            driver.quit();
+        }
+    }
     @Test
-    public void selectFromDbTest() throws SQLException, ClassNotFoundException {
-        connectToDB();
+    public void insertIntoDBTest() throws SQLException, ClassNotFoundException {
+        connectionToDB();
         try {
-            resultSet = statement.executeQuery("SELECT * FROM Persons");
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("FirstName"));
-                System.out.println(resultSet.getString("LastName"));
-            }
+            statement.execute("INSERT INTO IPhoneTable (NamePhone,PrisePhone)" +
+                    "VALUES ('IPhone15proS', '55000')");
         } finally {
             closeConnection();
         }
     }
-
-    @Test
-    public void insertIntoDbTest() throws SQLException, ClassNotFoundException {
-        connectToDB();
-        try {
-            ResponseDto responseDto = getPersons(10);
-            for (PersonDto personDto : responseDto.getResults()) {
-                try {
-                    statement.execute(String.format(INSERT_PATTERN,
-                            personDto.getName().getFirst(),
-                            personDto.getName().getLast(),
-                            personDto.getGender(),
-                            personDto.getName().getTitle(),
-                            personDto.getNat()));
-                } catch (Exception e) {
-                    System.out.println("Something went wrong for "
-                            + personDto.getName().getFirst() + " " + personDto.getName().getLast());
-                }
-            }
-        } finally {
-            closeConnection();
-        }
-    }
-
-    private void connectToDB() throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/db", "user", "password");
+    private void connectionToDB() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection =
+                DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "user", "password");
         statement = connection.createStatement();
     }
-
     private void closeConnection() throws SQLException {
         if (resultSet != null) {
             resultSet.close();
@@ -79,14 +64,25 @@ public class SqlTests {
             connection.close();
         }
     }
+    private String nameElementFoTable(String fName){
+        String devise = "iphone 15";
+        driver.get("https://allo.ua/");
+        WebElement searchBox = driver.findElement(By.name("search") );
+        searchBox.sendKeys(devise);
+        searchBox.sendKeys(Keys.ENTER);
 
-    private ResponseDto getPersons(int count) {
-        RequestSpecification requestSpecification = RestAssured.given();
-        requestSpecification.queryParam("inc", "gender,name,nat");
-        requestSpecification.queryParam("noinfo");
-        requestSpecification.queryParam("results", count);
-        requestSpecification.baseUri("https://randomuser.me/");
-        requestSpecification.basePath("/api");
-        return requestSpecification.get().as(ResponseDto.class);
+        WebDriverWait driverWait= new WebDriverWait(driver, Duration.ofSeconds(30L));
+
+        List<WebElement> searchPhoneElement=
+                driverWait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//img[contains(@src,'https://i.allo.ua/media/catalog/product/cache/3/small_image/212x184/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone_15')]"),4 ));
+        searchPhoneElement.get(0).click();
+
+        WebElement findName = driver.findElement(By.xpath("//h1[contains(@class,'p-view__header-title')]"));
+        WebElement nameElement=findName;
+        String nameEl=nameElement.getText();
+        System.out.println(nameEl );
+
+        return nameEl  ;
     }
 }
+
