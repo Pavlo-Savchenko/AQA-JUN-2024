11882df375501a0e9715ad0dad13aae950e02018
Pavlo-Savchenko <Spashok117@gmail.com>
2024-07-24 17:52:48 +0300

    DZ9FULL

--- src/test/java/org/prog/testng/SqlTests.java
+++ src/test/java/org/prog/testng/SqlTests.java
@@ -44,8 +44,8 @@ public class SqlTests {
         connectionToDB();
         try {
             String str= nameElementFoTable(driver);
-            statement.execute("INSERT INTO IPhoneTable (NamePhone,PrisePhone)" +
-                    "VALUES ('IPhone15proS', '55000')");
+            String str2= priseElementFoTable(driver);
+            statement.execute(String.format(INSERT_PATTERN,str,str2));
 
         } finally {
             closeConnection();
@@ -85,8 +85,27 @@ public class SqlTests {
         WebElement nameElement=findName;
         String nameEl=nameElement.getText();
         System.out.println(nameEl );
-return nameEl ;
+        return nameEl ;
+    }
+    public String priseElementFoTable(WebDriver  dr){
+        String devise = "iphone 15";
+        driver.get("https://allo.ua/");
+        WebElement searchBox = driver.findElement(By.name("search") );
+        searchBox.sendKeys(devise);
+        searchBox.sendKeys(Keys.ENTER);
+
+        WebDriverWait driverWait= new WebDriverWait(driver, Duration.ofSeconds(30L));
+
+        List<WebElement> searchPhoneElement=
+                driverWait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//img[contains(@src,'https://i.allo.ua/media/catalog/product/cache/3/small_image/212x184/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone_15')]"),4 ));
+        searchPhoneElement.get(0).click();
+
+        List<WebElement> searchPrice2=
+                driverWait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//span[contains(@class,'sum')]"),2));
+        System.out.println(searchPrice2.get(2).getText());
+        WebElement thirdElement=searchPrice2.get(2);
+        String secondPrice=thirdElement.getText();
+
+        return secondPrice ;
     }
 }
-
-
